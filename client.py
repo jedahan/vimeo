eef06a5270353b6f0e935735eec530eada40b37a
Jonathan Dahan <jonathan@jedahan.com>
2014-08-06 15:52:59 -0400

    unhash, rehash

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import requests
 import md5
 import struct
+import binascii
 
 url = "http://localhost:4242/api"
 
@@ -16,27 +17,42 @@ def pad_md5(l):
   padding = '\x80'
   padding += '\x00' * ((64 - len(length) - (l + 1) % 64) % 64)
   padding += length
-  print length
   return padding
 
-data = {
+old_data = {
     'method': 'vimeo.test.login',
     'api_key': raw_input("api_key: "),
 }
 
-flattened_req = flatten(data)
-flattened_req += pad_md5(len(flattened_req))
-
-# save current info in data
-data[flattened_req[0]] = flattened_req[1:]
+flattened_old = flatten(old_data)
+old_length = len(flattened_old)
+old_padding = pad_md5(old_length)
+flattened_old += old_padding
 
 # craft the packet we want
-data['video_id'] = '1337'
-data['favorite'] = '1'
-data['method'] = 'vimeo.videos.setFavorite'
-data['api_sig'] = md5.MD5(raw_input("api_sig: ")+flatten(data))
+new_data = {
+  'api_key': old_data['api_key'],
+  'video_id': '1337',
+  'favorite': '1',
+  'method': 'vimeo.videos.setFavorite',
+  flattened_old[0]: flattened_old[1:]
+}
 
-print data
+api_sig = raw_input("api_sig: ")
 
-r = requests.post(url, data=data)
+flattened_new = flatten(new_data)
+flattened_new += pad_md5(old_length + len(old_padding) + len(flattened_new))
+
+from md5 import MD5
+md5 = MD5('')
+md5.A, md5.B, md5.C, md5.D = struct.unpack('<IIII', binascii.unhexlify(api_sig))
+while len(flattened_new):
+    md5._handle(flattened_new[:64])
+    flattened_new = flattened_new[64:]
+
+new_data['api_sig'] = md5.hexdigest()
+
+print new_data
+
+r = requests.post(url, data=new_data)
 print r.text
