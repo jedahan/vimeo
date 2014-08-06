c0273d0dc40cbd9488d299c90a050b185fb9f49b
Jonathan Dahan <jonathan@jedahan.com>
2014-08-06 15:28:28 -0400

    updates for

--- client.py
+++ client.py
@@ -1,7 +1,23 @@
 import requests
 import md5
+import struct
 
 url = "http://localhost:4242/api"
+
+def flatten(req):
+  res = ""
+  for k, v in sorted(req.items()):
+    res += k
+    res += v
+  return res
+
+def pad_md5(l):
+  length = struct.pack('<Q', l * 8)
+  padding = '\x80'
+  padding += '\x00' * ((64 - len(length) - (l + 1) % 64) % 64)
+  padding += length
+  print length
+  return padding
 
 data = {
     'method': 'vimeo.test.login',
@@ -9,16 +25,17 @@ data = {
     'api_sig': raw_input("api_sig: "),
 }
 
+flattened_req = flatten(data)
+flattened_req += pad_md5(len(flattened_req))
+
 # save current info in data
-data['a'] = 'pi_key'+data['api_key']+'method'+data['method']
+data[flattened_req[0]] = flattened_req[1:]
 
 # craft the packet we want
 data['video_id'] = '1337'
 data['favorite'] = '1'
 data['method'] = 'vimeo.videos.setFavorite'
-
-bad_md5 = md5.MD5(data['api_sig']+'api_key'+data['api_key']+'favorite'+data['favorite']+'method'+data['method']+'video_id'+data['video_id'])
-data['api_sig'] = bad_md5.hexdigest()
+data['api_sig'] = md5.MD5(data['api_sig']+flatten(data))
 
 print data
 
